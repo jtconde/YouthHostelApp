71cf345cf6559eabb52411da87362d8b59e6b1af
jtconde <julianconde@Owners-MacBook-Pro.local>
2016-09-21 16:47:16 -0400

    Up

---
+++ app/src/main/java/com/ch/julianconde/youthhostelapp/HostelInfo.java
@@ -0,0 +1,13 @@
+package com.ch.julianconde.youthhostelapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HostelInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hostel_info);
+    }
+}
